99c6a2a306fac4d831142be3b8a46d88bd644d29
jhessiny <jhessinymattos@gmail.com>
2021-03-25 23:09:25 -0300

    layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="./logo.png" alt="" />
+    </header>
+  );
+};
+
+export default Header;
